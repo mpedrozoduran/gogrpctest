292a725b975942cb0fa6bbe907f5f5de0dd6f472
Miguel Pedrozo <miguel.pedrozo@avaldigitallabs.com>
2020-04-23 14:23:47 -0500

    added time manager server impl

--- timemanager/time.go
+++ timemanager/time.go
@@ -1,1 +1,14 @@
 package timemanager
+
+import (
+	"context"
+	pb "github.com/mpedrozoduran/gogrpctest/timeproto"
+)
+
+type TimeStruct struct {
+	pb.UnimplementedTimeManagerServer
+}
+
+func (t *TimeStruct) GetTime(ctx context.Context, request *pb.TimeRequest) (*pb.TimeResponse, error) {
+	return &pb.TimeResponse{ServerTime: "2020-04-23 14:17:00"}, nil
+}
